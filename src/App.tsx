1185551c8d38e29068cca0f60e9cca4144bd3699
farkmarnum <mark.farnum@gmail.com>
2020-10-13 15:37:14 -0400

    switch logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import logo from './logo.svg'
+import logo from './logo.jpg'
 import './App.css'
 
-function App() {
+const App = (): Element => {
   return (
     <div className="App">
       <header className="App-header">
