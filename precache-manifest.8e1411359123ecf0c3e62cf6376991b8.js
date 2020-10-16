bac43fe09f51c0b46afe19b31393a9947b02c3ba
farkmarnum <mark.farnum@gmail.com>
2020-10-16 11:06:08 -0400

    Updates

---
+++ precache-manifest.8e1411359123ecf0c3e62cf6376991b8.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56a09188fe1353ff4c218d0468f3e54",
+    "url": "/carbon-journey/index.html"
+  },
+  {
+    "revision": "3beed92a284e61cbcd81",
+    "url": "/carbon-journey/static/css/main.af80bcd7.chunk.css"
+  },
+  {
+    "revision": "caeac1a7a69386d52b5e",
+    "url": "/carbon-journey/static/js/2.c92f142b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/carbon-journey/static/js/2.c92f142b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be245c120cfcdb9533ae",
+    "url": "/carbon-journey/static/js/3.9bb4a58b.chunk.js"
+  },
+  {
+    "revision": "3beed92a284e61cbcd81",
+    "url": "/carbon-journey/static/js/main.e1f7dc86.chunk.js"
+  },
+  {
+    "revision": "4e8fcdef73845c02257f",
+    "url": "/carbon-journey/static/js/runtime-main.b6bb6021.js"
+  },
+  {
+    "revision": "1100d1f89fd659efb13d5673e8cb9c70",
+    "url": "/carbon-journey/static/media/atmo-plant.1100d1f8.png"
+  },
+  {
+    "revision": "ea5d24e5ea265b70fa78da587d77f62a",
+    "url": "/carbon-journey/static/media/atmo-stay.ea5d24e5.png"
+  },
+  {
+    "revision": "5529a4d385abc165d461a95620593323",
+    "url": "/carbon-journey/static/media/atmo-water.5529a4d3.png"
+  },
+  {
+    "revision": "08d0e11b3b772399e4be9f77dc6db269",
+    "url": "/carbon-journey/static/media/bio-atmo.08d0e11b.png"
+  },
+  {
+    "revision": "fd44ea7f9e274ba5ef15fe3cce653a60",
+    "url": "/carbon-journey/static/media/bio-lith.fd44ea7f.png"
+  },
+  {
+    "revision": "314c5778cab3a219cf0178ba9edb31b0",
+    "url": "/carbon-journey/static/media/bio-stay.314c5778.png"
+  },
+  {
+    "revision": "94591631da5436ffe240a65132e6e722",
+    "url": "/carbon-journey/static/media/hydro-atmo.94591631.png"
+  },
+  {
+    "revision": "2a63d6cec73c5f93e82aaf958aaf88ac",
+    "url": "/carbon-journey/static/media/hydro-bio.2a63d6ce.png"
+  },
+  {
+    "revision": "bc51b8f74ce10b23b5f487834e4d0e37",
+    "url": "/carbon-journey/static/media/hydro-litho.bc51b8f7.png"
+  },
+  {
+    "revision": "3c886bbd0bef05c5ff26259a94613393",
+    "url": "/carbon-journey/static/media/hydro-stay.3c886bbd.png"
+  },
+  {
+    "revision": "6d39d2fd60b5941b9797aefe00ac664e",
+    "url": "/carbon-journey/static/media/nx.6d39d2fd.png"
+  },
+  {
+    "revision": "14fb552a928a3da04d25cc8e0f1a8067",
+    "url": "/carbon-journey/static/media/ny.14fb552a.png"
+  },
+  {
+    "revision": "b20345671b4e7fbef58a6c99eee6af75",
+    "url": "/carbon-journey/static/media/nz.b2034567.png"
+  },
+  {
+    "revision": "6d9af7bb63b6bac82e765e7e53bfbdeb",
+    "url": "/carbon-journey/static/media/postlitho-atmo.6d9af7bb.png"
+  },
+  {
+    "revision": "71a49219e68657adf5e04be0a5228114",
+    "url": "/carbon-journey/static/media/postlitho-stay.71a49219.png"
+  },
+  {
+    "revision": "b9cb90d37a54a232981c62fa85981ccd",
+    "url": "/carbon-journey/static/media/prelitho-atmo.b9cb90d3.png"
+  },
+  {
+    "revision": "41a347e33355e4294cc040fa616d205f",
+    "url": "/carbon-journey/static/media/prelitho-stay.41a347e3.png"
+  },
+  {
+    "revision": "941fa3c080155f93f6da812a2736fdcf",
+    "url": "/carbon-journey/static/media/px.941fa3c0.png"
+  },
+  {
+    "revision": "44b672f335462dd052ace54cf7e6d663",
+    "url": "/carbon-journey/static/media/py.44b672f3.png"
+  },
+  {
+    "revision": "60b2f35e9bf87f01180de86a3c4ec370",
+    "url": "/carbon-journey/static/media/pz.60b2f35e.png"
+  }
+]);
